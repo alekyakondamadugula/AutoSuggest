ac0ebb5e2122935a43a3d48dce5cb0d1fc0ad8e4
Alekya Kondamadugula <kondamadugulaalekya@gmail.com>
2026-07-03 18:55:36 +0530

    Toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,30 @@
-console.log("JavaScript Connected!");
+const users = [
+    {
+        name: "Jane Doe",
+        gender: "Female",
+        image: "images/jane.png"
+    },
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "images/john.png"
+    }
+];
 
-const button = document.getElementById("btn");
+let curUserId = 0;
 
-button.addEventListener("click", function () {
-    alert("Button Clicked!");
-});
+function toggle() {
+
+    if(curUserId === 0){
+        curUserId = 1;
+    }
+    else{
+        curUserId = 0;
+    }
+
+    document.getElementById("img").src = users[curUserId].image;
+
+    document.getElementById("card-name").innerText = users[curUserId].name;
+
+    document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
